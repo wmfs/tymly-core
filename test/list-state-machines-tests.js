a4ada368549bc275055f02c491c9ec8ed437f5ef
Meena.Hoda <meena.hoda@wmfs.net>
2018-11-15 11:00:18 +0000

    test: update tests in line with statebox changes

--- test/list-state-machines-tests.js
+++ test/list-state-machines-tests.js
@@ -47,12 +47,8 @@ describe('list available state machines', () => {
   it('available-state-machines state resource', async () => {
     const stateMachines = await runAvailableStateMachines()
 
-    expect(stateMachines).to.eql([
-      {
-        name: 'tymlyTest_timestamp_1_0',
-        description: 'Blueprint to get timestamp'
-      }
-    ])
+    expect(stateMachines[0].name).to.eql('tymlyTest_timestamp_1_0')
+    expect(stateMachines[0].description).to.eql('Blueprint to get timestamp')
   })
 
   after('shutdown tymly', async () => {
