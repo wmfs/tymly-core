b3fab75d2214a905114d9f9b759f8dc408f98821
Jez Higgins <jez@jezuk.co.uk>
2019-03-11 11:31:26 +0000

    test(tymlyRef): Further tests

--- test/tymlyRef-resolution-tests.js
+++ test/tymlyRef-resolution-tests.js
@@ -5,37 +5,64 @@ const path = require('path')
 const expect = require('chai').expect
 
 describe('TymlyRef resolution', () => {
-  it('pre-boot reference resolution', async () => {
-    const tymlyServices = await tymly.boot({
-      pluginPaths: [
-        path.resolve(__dirname, 'fixtures/plugins/with-ref-plugin')
+  const probePluginPath = path.resolve(__dirname, 'fixtures/plugins/with-ref-plugin')
+  const bpDir = path.resolve(__dirname, 'fixtures/blueprints')
+
+  const inner = {
+    blueprintName: 'withRef',
+    blueprintVersion: '1.0',
+    namespace: 'refResolution',
+    id: 'inner',
+    name: 'inner'
+  }
+
+  const tests = [
+    [
+      'good reference',
+      'with-good-ref-blueprint',
+      inner
+    ],
+    [
+      'good reference in an array',
+      'with-good-ref-in-array-blueprint',
+      [
+        'some stuff before',
+        inner,
+        'some stuff afterwards'
       ]
-    })
-
-    const files = tymlyServices.probe.files
-
-    expect(Object.keys(files).length).to.eql(2)
-    expect(files.refResolution_inner).to.eql({
-      blueprintName: 'withRef',
-      blueprintVersion: '1.0',
-      namespace: 'refResolution',
-      id: 'inner',
-      name: 'inner'
-    })
-    expect(files.refResolution_outer).to.eql({
-      blueprintName: 'withRef',
-      blueprintVersion: '1.0',
-      namespace: 'refResolution',
-      id: 'outer',
-      name: 'outer',
-      description: 'a file which has a tymly ref to another file',
-      contents: {
-        blueprintName: 'withRef',
-        blueprintVersion: '1.0',
-        namespace: 'refResolution',
-        id: 'inner',
-        name: 'inner'
+    ],
+    [
+      'reference the same file twice',
+      'with-two-good-refs-blueprint', {
+        once: inner,
+        again: inner
       }
+    ]
+  ]
+
+  for (const [label, bp, contents] of tests) {
+    it(label, async () => {
+      const tymlyServices = await tymly.boot({
+        pluginPaths: [ probePluginPath ],
+        blueprintPaths: [ path.resolve(bpDir, bp) ]
+      })
+
+      const files = tymlyServices.probe.files
+
+      expect(Object.keys(files).length).to.eql(2)
+      expect(files.refResolution_outer.contents).to.eql(contents)
     })
+  }
+
+  it('bad reference', async () => {
+    try {
+      await tymly.boot({
+        blueprintPaths: [
+          path.resolve(bpDir, 'with-bad-ref-blueprint')
+        ]
+      })
+    } catch (err) {
+      expect(err.message).to.eql('Error: Could not resolve \'files:refResolution_inner\' in refResolution_outer')
+    }
   })
 })
