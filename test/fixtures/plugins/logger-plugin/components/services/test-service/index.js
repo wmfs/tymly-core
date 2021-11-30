a60a9e48e02789917aac7b21e9b869cb10ea9d27
meenahoda <meena.hoda@wmfs.net>
2021-11-30 07:46:00 +0000

    test: illustrate use of namespace when adding logger

--- test/fixtures/plugins/logger-plugin/components/services/test-service/index.js
+++ test/fixtures/plugins/logger-plugin/components/services/test-service/index.js
@@ -1,13 +1,19 @@
 class TestService {
   boot (options) {
-    const logger = options.bootedServices.logger.addLogger('test-service')
-    // const logger = options.bootedServices.logger.child({ namespace: 'test-service' })
-    logger.trace('This is a trace message')
-    logger.debug('This is a debug message')
-    logger.info('This is an info message')
-    logger.warn('This is a warn message')
-    logger.error('This is an error message')
-    logger.fatal('This is a fatal message')
+    const logger1 = options.bootedServices.logger.addLogger('test-service')
+
+    logger1.trace('This is a trace message')
+    logger1.debug('This is a debug message')
+
+    const logger2 = options.bootedServices.logger.addLogger('testing')
+
+    logger2.info('This is an info message')
+    logger2.warn('This is a warn message')
+
+    const logger3 = options.bootedServices.logger.addLogger()
+
+    logger3.error('This is an error message')
+    logger3.fatal('This is a fatal message')
   }
 }
 
