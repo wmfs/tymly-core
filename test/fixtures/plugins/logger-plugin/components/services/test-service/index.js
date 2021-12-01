1fc8697aaaf780f1bfc5738518c506cba01ba93a
meenahoda <meena.hoda@wmfs.net>
2021-12-01 08:43:58 +0000

    feat: refactor logger to use child

--- test/fixtures/plugins/logger-plugin/components/services/test-service/index.js
+++ test/fixtures/plugins/logger-plugin/components/services/test-service/index.js
@@ -1,16 +1,16 @@
 class TestService {
   boot (options) {
-    const logger1 = options.bootedServices.logger.addLogger('test-service')
+    const logger1 = options.bootedServices.logger.child('test-service')
 
-    logger1.trace('This is a trace message')
+    logger1.trace('This', 'is', 'a', 'trace', 'message')
     logger1.debug('This is a debug message')
 
-    const logger2 = options.bootedServices.logger.addLogger('testing')
+    const logger2 = options.bootedServices.logger.child({ namespace: 'testing' })
 
     logger2.info('This is an info message')
     logger2.warn('This is a warn message')
 
-    const logger3 = options.bootedServices.logger.addLogger()
+    const logger3 = options.bootedServices.logger.child()
 
     logger3.error('This is an error message')
     logger3.fatal('This is a fatal message')
